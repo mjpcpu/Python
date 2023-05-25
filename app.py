dcfa5618a443146670588222db30eeb3dbe79db8
Mike Pemberton <mjpcpu@gmail.com>
2023-05-25 23:25:20 +0100

    Initial Commit

---
+++ app.py
@@ -0,0 +1,12 @@
+# name = input("What is your name? ")
+# print("Hello " + name)
+
+# birth_year = input("Enter your birth year")
+# age = 2023 - int(birth_year)
+# print(age)
+
+# first = input("First Number: ")
+# second = input("Second Number: ")
+# sum = float(first) + float(second)
+# print("Sum: " + str(sum))
+
